838b2109a52b9722b1a78608fef0b5a339c5e4de
Diego Merello <diego@wipitalia.it>
2021-05-22 17:35:51 +0200

    lights and shadows

--- components/Stage.js
+++ components/Stage.js
@@ -1,23 +1,26 @@
 import {Canvas} from "@react-three/fiber";
+import {Box} from "@react-three/drei";
 import Gallery from '@/components/Gallery';
 import Camera from '@/components/Camera';
 import Floor from '@/components/Floor';
 
 const Stage = ({contents}) => {
 
-  const ambientColor = '#000';
+  const ambientColor = '#111316';
   const cameraPosition = [0, 1, 15];
 
   return (
     <div className="stage">
-      <Canvas shadowMap>
+      <Canvas shadows={true} shadowMap>
         <color attach="background" args={ambientColor} />
         <fog attach="fog" args={[ambientColor, 0, 30]} />
         <ambientLight castShadow intensity={1}/>
-        <spotLight intensity={3} position={[2, 5, 15]} angle={0.4} penumbra={1} castShadow />
-        <spotLight intensity={3} position={[-2, 5, 15]} angle={0.4} penumbra={1} castShadow />
+        <spotLight intensity={0.5} position={[-2, 5, 10]} angle={2} penumbra={1} castShadow />
+        <spotLight intensity={1} position={[2, 0, 10]} angle={2} penumbra={1} castShadow />
+        <spotLight intensity={2} position={[0, 30, 30]} angle={2} penumbra={1} castShadow />
         <Gallery contents={contents} cameraPosition={cameraPosition}/>
         <Camera position={cameraPosition}/>
+        <Box args={[1,1,1]} rotation={[0,10,0]} castShadow position={[0,0,0]}/>
         <Floor color={ambientColor}/>
       </Canvas>
     </div>
